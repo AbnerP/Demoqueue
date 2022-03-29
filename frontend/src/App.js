4010ef62531bbb83f61ed0ec1dfa11a43c9a1100
Abner Pena <abnrr.pena@gmail.com>
2022-03-29 15:47:18 -0400

    Material UI for upvote and downvote icons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,30 @@
+import { createTheme, ThemeProvider } from "@material-ui/core";
+import { green } from "@material-ui/core/colors";
 import { BrowserRouter, Link, Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import './App.css';
 import LangdingPage from "./Screens/Landing";
 import LiveQueue from "./Screens/LiveQueue";
 import SessionCode from "./Screens/SessionCode";
+const theme = createTheme({
+  palette:{
+    success: {
+      main:"#4BB543",
+    }
+  }
+});
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={ <Navigate to="/home"/>} />
-        <Route path="/home" element={<LangdingPage />} />
-        <Route path="/queue" element={<LiveQueue />} />
-        <Route path="/code" element={<SessionCode />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={ <Navigate to="/home"/>} />
+          <Route path="/home" element={<LangdingPage />} />
+          <Route path="/queue" element={<LiveQueue />} />
+          <Route path="/code" element={<SessionCode />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- frontend/src/Components/SongSuggestion/SongSuggestion.js
+++ frontend/src/Components/SongSuggestion/SongSuggestion.js
@@ -1,24 +1,39 @@
-import React, { useState } from 'react'
-import "./SongSuggestion.css"
-function SongSuggestion(props) {
-  
-  return (
-    <div className='song__card'>
-        <div className="song__info">
-          <h3>{props.name}</h3>
-          <h4>{props.artist}</h4>
-        </div>
-        
-        <div className="song__interactions">
-          <h4>{props.votes} votes</h4>  
+import React, { useState } from "react";
+import "./SongSuggestion.css";
+import { IconButton } from "@material-ui/core";
+import { ArrowDropUp, ArrowDropDown } from "@material-ui/icons";
 
-          <div className="song__interactions--voting">
-            <button onClick={() => props.upVote(props.index)}>^</button>
-            <button onClick={() => props.downVote(props.index)}>v</button>
-          </div>
+function SongSuggestion(props) {
+  return (
+    <div className="song__card">
+      <div className="song__info">
+        <h3>{props.name}</h3>
+        <h4>{props.artist}</h4>
+      </div>
+
+      <div className="song__interactions">
+        <div className="votes__counter--container">
+          <span className="song__votes song__votes--number">{props.votes}</span>
+          <span className="song__votes song__votes--text">votes</span>
         </div>
+
+        <div className="song__interactions--voting">
+          <IconButton
+            style={{ color: "#4BB543"}}
+            onClick={() => props.upVote(props.index)}
+          >
+            <ArrowDropUp style={{ transform:"scale(1.5)"}}/>
+          </IconButton>
+          <IconButton
+            style={{ color: "#eb5534"}}
+            onClick={() => props.downVote(props.index)}
+          >
+            <ArrowDropDown style={{ transform:"scale(1.5)"}}/>
+          </IconButton>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SongSuggestion
+export default SongSuggestion;
