65243a4e823fb15dfbfd9b09a7777ab7c008169a
Abner Pena <abnrr.pena@gmail.com>
2022-03-30 15:03:52 -0400

    #9 Sorting List Option

--- frontend/src/Helpers/sort.js
+++ frontend/src/Helpers/sort.js
@@ -1,5 +1,13 @@
-export default function sortAndReturn (queue) {
+export default function sortAndReturnNumerically (queue) {
     return queue.sort((a, b) => {
         return b.votes - a.votes;
+    });
+}
+
+export function sortAndReturnAlphabetically (queue) {
+    return queue.sort((a, b) => {
+        if( a.name < b.name) return -1;
+        if( a.name > b.name) return 1;
+        return 0;
     });
 }
