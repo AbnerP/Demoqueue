e04f3ac92482bde3fda59ce655847c7c8670a5a1
edwindang <edang@u.rochester.edu>
2022-04-07 12:45:54 -0400

    qrcode

--- frontend/src/Screens/SessionCode.js
+++ frontend/src/Screens/SessionCode.js
@@ -1,12 +1,46 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import {useEffect, useState} from 'react';
+import "./SessionCode.css"
 
-function SessionCode() {
+function SessionCode(props) {
+  const id = props.id;
+  const [temp, setTemp] = useState("");
+  const [url, setUrl] = useState("https://www.google.com/");
+  const [size] = useState(400);
+  const [qrCode, setQrCode] = useState("");
+
+  useEffect(() => {
+    setQrCode
+ (`http://api.qrserver.com/v1/create-qr-code/?data=${url}!&size=${size}x${size}`);
+  }, [url, size]);
+
+  function handleClick() {
+    setUrl(temp);
+  }
+
   const navigate = useNavigate();
   return (
-    <div>
-      <h1>SessionCode</h1>
+    <div className='main'>
+      <h1>Current Session Code</h1>
       <button onClick={() => navigate("/queue")}>Back</button>
+      <div className="input-box">
+        <div className="gen">
+          <input type="text" onChange={
+            (e) => {setTemp(e.target.value)}}
+            placeholder="Enter queue #" />
+          <button className="button" 
+            onClick={handleClick}>
+            Generate
+          </button>
+        </div>
+      </div>
+      <div className="output-box">
+        <img src={qrCode} alt="" />
+        <a href={qrCode} download="QRCode">
+          <button type="button" className='download-button'>Download</button>
+        </a>
+      </div>
     </div>
   );
 }
